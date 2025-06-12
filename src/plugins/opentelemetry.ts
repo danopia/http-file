00c8d2cc3b00018fc9ac5c3c90fc1690f68aae45
Daniel Lamando <dan@danopia.net>
2025-06-12 15:18:30 +0200

    Try out module docs for the plugins

--- src/plugins/opentelemetry.ts
+++ src/plugins/opentelemetry.ts
@@ -1,3 +1,20 @@
+/**
+ * Wraps the http file's steps and tests with OpenTelemetry spans.
+ * Best used with Deno's native otel support to export these spans
+ * to your observability tool of choice.
+ *
+ * You may find it helpful to have trace deeplinks included in the output.
+ * To enable this, set the environment variable OTEL_TRACE_URL_TEMPLATE
+ * The value will depend on your particular telemetry vendor.
+ *
+ * Example values, replacing <...> as appropriate:
+ * - Datadog: https://<datadog-site>/apm/trace/{TraceId}?graphType=flamegraph&spanID={SpanIdDecimal}
+ * - Jaeger: https://<some-jaeger-instance>/trace/{TraceId}?uiFind={SpanId}
+ * - Honeycomb: https://ui.honeycomb.io/<team>/environments/<env>/trace?trace_id={TraceId}&span={SpanId}
+ *
+ * @module
+ */
+
 import { LogicTracer } from '@cloudydeno/opentelemetry/instrumentation/async.ts';
 import type { Span } from '@cloudydeno/opentelemetry/pkg/api';
 
@@ -21,11 +38,7 @@ const testTracer = new LogicTracer({
   requireParent: true,
 });
 
-/**
- * Wraps the http file's steps and tests with OpenTelemetry spans.
- * Best used with Deno's native otel support to export these spans
- * to your observability tool of choice.
- */
+/** Client plugin which wraps files, steps, and tests with OpenTelemetry spans. */
 export const plugin: PluginRegistration = {
   name: 'opentelemetry',
   denoFlags: ['--unstable-otel'],
@@ -63,16 +76,13 @@ export const plugin: PluginRegistration = {
 
 /**
  * Print a trace viewer deeplink if configured via enviroment.
- * Example template values, replacing <...> as appropriate:
- * - Datadog: https://<datadog-site>/apm/trace/{TraceId}?graphType=flamegraph&spanID={SpanIdDecimal}
- * - Jaeger: https://<some-jaeger-instance>/trace/{TraceId}?uiFind={SpanId}
- * - Honeycomb: https://ui.honeycomb.io/<team>/environments/<env>/trace?trace_id={TraceId}&span={SpanId}
  * @param span The particular span to deeplink to
  */
 function maybePrintTraceLink(span: Span | null) {
   const urlTemplate = Deno.env.get('OTEL_TRACE_URL_TEMPLATE');
   if (urlTemplate && span) {
     const ctx = span.spanContext();
+    // Not a very solid heuristic
     if (ctx.traceId == '00000000000000000000000000000000') {
       console.log(`\nTracing of this script run was not recorded.\n`);
     } else {
